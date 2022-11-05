eeea6f6dfd740de90c742d7757638297468079fd
Konstantin <kain7984@gmail.com>
2022-11-05 21:31:30 +0800

    New branch lambda. Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -17,7 +18,29 @@ class Main {
 
     public static void main(String[] args) {
         List<Person> person = personList();
-        person.sort(new SurnameAgeComparator(1));
+
+        Comparator<Person> newComporator = (Person o1, Person o2) -> {
+            int maxSurnameWords = 1;
+            String[] wordsArray = o1.getSurname().split("\\P{IsAlphabetic}+");
+            int firstObjectLength = wordsArray.length;
+
+            wordsArray = o2.getSurname().split("\\P{IsAlphabetic}+");
+            int secondObjectLength = wordsArray.length;
+
+            if (firstObjectLength > maxSurnameWords && secondObjectLength > maxSurnameWords) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            } else {
+                if (firstObjectLength < secondObjectLength) {
+                    return -1;
+                } else if (firstObjectLength > secondObjectLength) {
+                    return 1;
+                } else {
+                    return Integer.compare(o1.getAge(), o2.getAge());
+                }
+            }
+        };
+
+        person.sort(newComporator);
         System.out.println(person);
     }
 }
